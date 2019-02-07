2b0d7e65f155b0398e05c2bb1a9aaa2b21e57971
saraansh <bsaraansh@gmail.com>
2019-02-07 20:27:58 +0530

    Edit Post with Image-view added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,24 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def create
 		@post = Post.create(post_params)
 		session[:id] = @post.id
 		redirect_to posts_path
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to post_path(@post)
 	end
 	
 	private
